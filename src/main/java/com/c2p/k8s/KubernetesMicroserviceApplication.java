2cb89ca4b403f38fc89b0a95494a0acaca22c4bf
surjeetmohanty <surjeetmohanty2019@gmail.com>
2020-04-04 17:16:43 +0530

    File changed

--- src/main/java/com/c2p/k8s/KubernetesMicroserviceApplication.java
+++ src/main/java/com/c2p/k8s/KubernetesMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.c2p.k8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class KubernetesMicroserviceApplication {
 
 	public static void main(String[] args) {
